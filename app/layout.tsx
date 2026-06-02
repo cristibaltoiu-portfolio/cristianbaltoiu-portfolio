c4f1fc0f9db1906659836e21073c21a363a38916
Cristian Baltoiu <cristibaltoiu@yahoo.com>
2026-06-03 02:09:36 +0300

    Fix BackToTop import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import BackToTop from './components/BackToTop'
+import { BackToTop } from './components/BackToTop'
 
 const inter = Inter({ subsets: ['latin'] })
 
